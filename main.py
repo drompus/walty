24823687de9245a91f2d4c3bfbf928f299697f6c
drompus <163110258+drompus@users.noreply.github.com>
2024-05-06 14:29:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,7 @@ Siema, jestem Dominik. Mieszkam w Polsce. Chodze do pierwszej klasy technikum
 
 """
 
+https://prod.liveshare.vsengsaas.visualstudio.com/join?8CBC3A82B63DFBBCFF2E559C82A2D6C37CD5
+
+
 print(ABOUT_DROMPUS)
